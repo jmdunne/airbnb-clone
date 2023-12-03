48558c1a053931b00db73d386c044a50ed0d0a35
Josh Dunne <joshua.m.dunne@gmail.com>
2023-12-03 11:12:20 -0700

    Layout, Navigation, and Container Components

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import Container from '../Container'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='fixed w-full bg-white z-10 shadow-sm'>
+        <div className='
+           py-4
+           border-b-[1px]
+           '
+        >
+        <Container>
+            Anything
+        </Container>    
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
