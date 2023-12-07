0e05eaff38daf0f06f8e228ae74b39a4650d9dea
Josh Dunne <joshua.m.dunne@gmail.com>
2023-12-07 15:33:54 -0700

    Created heading and placed it in registermodal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -8,6 +8,8 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../Inputs.tsx/Input";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -41,6 +43,13 @@ const RegisterModal = () => {
       });
   };
 
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
+      <Input />
+    </div>
+  );
+
   return (
     <Modal
       disabled={isLoading}
@@ -49,6 +58,7 @@ const RegisterModal = () => {
       actionLabel="Continue"
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
     />
   );
 };
