e576172df729fef9c88db9c969f0cb5e2aa86b32
Josh Dunne <joshua.m.dunne@gmail.com>
2023-12-19 16:52:03 -0700

    Update GitHub authentication credentials

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -12,6 +12,7 @@ import Heading from "../Heading";
 import Input from "../Inputs.tsx/Input";
 import toast from "react-hot-toast";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -89,7 +90,7 @@ const RegisterModal = () => {
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div
         className="
