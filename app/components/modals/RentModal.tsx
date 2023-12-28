6d4dc5122a205112fdd5f4ad9ad0b1952f955d7a
Josh Dunne <joshua.m.dunne@gmail.com>
2023-12-28 13:39:55 -0700

    Add CategoryInput component to RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,6 +6,7 @@ import Modal from "./Modal";
 import { use, useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
+import CategoryInput from "../Inputs.tsx/CategoryInput";
 
 enum STEPS {
   CATEGORY = 0,
@@ -52,9 +53,14 @@ const RentModal = () => {
         subtitle="Pick a category"
       />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
-        {categories.map((items) => (
-          <div key={items.label} className="col-span-1">
-            {items.label}
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={() => {}}
+              selected={false}
+              label={item.label}
+              icon={item.icon}
+            />
           </div>
         ))}
       </div>
