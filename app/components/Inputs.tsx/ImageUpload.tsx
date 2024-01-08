613e59c8f5837c5570304bf67376849b95903370
Josh Dunne <joshua.m.dunne@gmail.com>
2024-01-08 16:54:01 -0700

    Added cloudinary preset

--- app/components/Inputs.tsx/ImageUpload.tsx
+++ app/components/Inputs.tsx/ImageUpload.tsx
@@ -25,7 +25,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
   return (
     <CldUploadWidget
       onUpload={handleUpload}
-      uploadPreset=""
+      uploadPreset="nfezi0np"
       options={{
         maxFiles: 1,
       }}
@@ -51,6 +51,16 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
           >
             <TbPhotoPlus size={50} />
             <div className="font-semibold text-lg">Click to upload</div>
+            {value && (
+              <div className="absolute inset-0 w-full h-full">
+                <Image
+                  alt="Upload"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={value}
+                />
+              </div>
+            )}
           </div>
         );
       }}
