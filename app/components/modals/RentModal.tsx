0f8683c3006ef330bc87a425494ef9e701de713a
Josh Dunne <joshua.m.dunne@gmail.com>
2024-01-10 15:24:17 -0700

    Add input field for title in RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -13,6 +13,7 @@ import CountrySelect from "../Inputs.tsx/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../Inputs.tsx/Counter";
 import ImageUpload from "../Inputs.tsx/ImageUpload";
+import Input from "../Inputs.tsx/Input";
 
 enum STEPS {
   CATEGORY = 0,
@@ -27,6 +28,8 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -174,6 +177,25 @@ const RentModal = () => {
         <ImageUpload
           value={imageSrc}
           onChange={(value) => setCustomValue("imageSrc", value)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place?"
+          subtitle="Short and sweet is best"
+        />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
         />
       </div>
     );
