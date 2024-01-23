a0ec4fbce7f3f7e037325d25f75755893a2a010f
Josh Dunne <joshua.m.dunne@gmail.com>
2024-01-23 16:26:16 -0700

    Add currentUser prop to ListingCard component

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,6 +1,26 @@
+import { SafeUser } from "@/app/types";
+import { Listing, Reservation } from "@prisma/client";
 import React from "react";
 
-const ListingCard = () => {
+interface ListingCardProps {
+  data: Listing;
+  reservation?: Reservation;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  reservation,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId,
+  currentUser,
+}) => {
   return <div>ListingCard</div>;
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
@@ -6,6 +7,7 @@ import EmptyState from "./components/navbar/EmptyState";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
@@ -31,7 +33,13 @@ export default async function Home() {
         gap-8"
         >
           {listings.map((listing: any) => {
-            return <ListingCard key={listing.id} data={listing} />;
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
           })}
         </div>
       </Container>
