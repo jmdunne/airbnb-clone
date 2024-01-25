6511ad892e79585b683f9a143a554ab56e3ff5d6
Josh Dunne <joshua.m.dunne@gmail.com>
2024-01-25 15:46:05 -0700

    Add Button component to ListingCard

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -10,6 +10,7 @@ import React, { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 // Define the props for the ListingCard component
 interface ListingCardProps {
@@ -115,6 +116,36 @@ const ListingCard: React.FC<ListingCardProps> = ({
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
+        <div
+          className="
+            font-semibold
+            text-lg
+            "
+        >
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div
+          className="
+        flex
+        flex-row
+        items-center
+        gap-1
+        "
+        >
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
     </div>
   );
